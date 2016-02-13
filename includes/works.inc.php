2279917c9c09d8fec2ed1d13134c5ccd26e32672
Jesse Dillman <jesse@jdillman.com>
2016-02-12 18:31:07 -0600

    initial markup and style

--- includes/works.inc.php
+++ includes/works.inc.php
@@ -1,22 +1,21 @@
 <section class="whitebg works">
-<!-- 550 800 550 -->
-	<div class="edge-item topworkz animate">
+	<div class="edge-item imgbg topworkz animate">
 		<h2>Topworkz</h2>
 	</div>
-	<div class="center-item joint animate">
+	<div class="center-item imgbg joint animate">
 		<h2>Joint</h2>
 	</div>
-	<div class="edge-item market animate">
+	<div class="edge-item imgbg market animate">
 		<h2>Market</h2>
 	</div>
 
-	<div class="edge-item worktop animate">
+	<div class="edge-item imgbg worktop animate">
 		<h2>Worktop</h2>
 	</div>
-	<div class="center-item lable animate">
+	<div class="center-item imgbg lable animate">
 		<h2>Label</h2>
 	</div>
-	<div class="edge-item kyiv animate">
+	<div class="edge-item imgbg kyiv animate">
 		<h2>KYIV</h2>
 	</div>
 </section>
